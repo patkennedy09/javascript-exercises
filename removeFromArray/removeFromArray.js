e1f77beb066411135b69eda981168f500187f849
Patrick Kennedy <patkennedy09@gmail.com>
2019-10-04 17:53:12 -0400

    finished removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
 
+    for (let j=1; j < arguments.length; j++) {
+
+        for (let i=0; i < arr.length; i++) {
+            if (arr[i] === arguments[j]) {
+                arr.splice(i, 1);
+            }
+        }
+    }
+    return arr
 }
 
 module.exports = removeFromArray
