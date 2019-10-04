32474df316ebce3963496822428cde97fb3c7369
Patrick Kennedy <patkennedy09@gmail.com>
2019-10-03 21:05:32 -0400

    finished helloworld and repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let final = ''
+    if (num < 0) {
+        return 'ERROR'
+    }
+    for (let i = 0; i < num; i++) {
+        final += str
+    }
+    return final
 }
 
 module.exports = repeatString
