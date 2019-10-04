2e2f06ae832b4ee9a3c98c493729cb356581cb1b
Patrick Kennedy <patkennedy09@gmail.com>
2019-10-03 21:52:49 -0400

    working reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    //first split string into an array
+    inarr = str.split("");
+    outarr = [];
+    for (let i=0; i < (inarr.length); i++) {
+        outarr[i] = inarr[inarr.length - 1 - i];
+    }
+    return outarr.join("");
 }
 
 module.exports = reverseString
