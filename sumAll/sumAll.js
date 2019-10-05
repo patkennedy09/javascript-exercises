22b392d8069e986546711624e12bb17e0c37cc3e
Patrick Kennedy <patkennedy09@gmail.com>
2019-10-05 09:36:27 -0400

    finished sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(low, high) {
+  if (typeof low != "number" || typeof high != "number" || low < 0 || high < 0) {
+    return "ERROR"
+  } else {
+    if (low > high) {
+        let temp = low;
+        low = high;
+        high = temp;
+    }
+    let sum = 0;
+    for (let i = low; i <= high; i++) {
+        sum += i;
+    }
+    return sum;
+  }
 }
 
 module.exports = sumAll
