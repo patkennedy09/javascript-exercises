68d62b755108ef71bf50c58061a74b5d8a97d88a
Patrick Kennedy <patkennedy09@gmail.com>
2019-10-26 13:49:46 -0400

    finished getTheTitles

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    //books is an array of objects
+    return books.map((book) => { return book.title; });
 }
 
 module.exports = getTheTitles;
