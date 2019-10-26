b1ffe99836f7039be7788637783756211d7d16d2
Patrick Kennedy <patkennedy09@gmail.com>
2019-10-26 14:25:58 -0400

    finished findTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+    //people is an array of objects
+    let oldest = people[0];
+    let oldAge = 0;
+    people.forEach((person) => {
+        if (!person.yearOfDeath) {
+            d = new Date();
+            person.yearOfDeath = d.getFullYear();
+        }
 
+        let age = person.yearOfDeath - person.yearOfBirth;
+        if (age > oldAge) {
+            oldest = person;
+            oldAge = age;
+        }
+    });
+    return oldest;
 }
 
 module.exports = findTheOldest
