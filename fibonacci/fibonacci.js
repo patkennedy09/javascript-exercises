73f8c24e721433a63c3a1cb5fe41a5d8175463e4
Patrick Kennedy <patkennedy09@gmail.com>
2019-10-26 12:44:58 -0400

    finished fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,4 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (typeof n != "number") {
+        n = Number(n);
+    }
+    
+    if (n < 1) {
+        return "OOPS";
+    }
+    let a = 0;
+    let b = 1;
+    let fib = 1;
+    for (let i=2; i <= n; i++) {
+        fib = a + b;
+        a = b;
+        b = fib;
+    }
+    
+    return fib;
 
 }
 
